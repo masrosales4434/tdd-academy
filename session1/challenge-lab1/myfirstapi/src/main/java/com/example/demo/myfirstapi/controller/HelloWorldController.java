c4971d38c705ae2250292102a81bbbb45cd3f74a
marvinandrew.rosales <marvinandrew.rosales@gcash.com>
2025-07-16 14:56:07 +0800

    Final Submission, Challenge lab 1

--- session1/challenge-lab1/myfirstapi/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
+++ session1/challenge-lab1/myfirstapi/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -25,30 +25,4 @@ public class HelloWorldController {
     public String greetUser(@PathVariable String name){
         return "Greetings " + name + "!";
     }
-
-    @GetMapping("/greeting")
-    public String greetWithParam(
-            @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-        return "Hello, " + name + "!";
-    }
-
-    @GetMapping("/calculate/sum")
-    public String sumNumbers   (@RequestParam("num1") int number1, @RequestParam("num2") int number2){
-        int sum = number1 + number2;
-        return "The sum of " + number1 +" and " + number2 + " is " + sum;
-    }
-
-    @GetMapping("/info")
-    public Map<String, String> getAppInfo(){
-        Map<String, String> info = new HashMap<>();
-        info.put("appName", "MyFirstSpringbootAPI");
-        info.put("version", "1.0.0");
-        info.put("status", "Running");
-        return info;
-    }
-
-    @GetMapping("/features")
-    public List<String> getFeatures(){
-        return Arrays.asList("REST API", "Spring Boot", "Easy Setup", "Fast Development");
-    }
 }
